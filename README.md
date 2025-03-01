35ed2017d280378aab0c757c328bba424e6fbc03
itz-enn <naimat.ife@gmail.com>
2025-03-01 21:55:00 +0100

    employee managemnt system

---
+++ README.md
@@ -0,0 +1,18 @@
+To initialize the project with a package.json file:
+npm init -y
+
+To install the dependencies in the project run:
+npm install express body-parser nodemon ejs
+
+To start the server:
+node index.js OR nodemon index.js
+
+To view the server:
+http://localhost:3000/
+
+http://localhost:3000/employees
+
+
+To run this project after cloning it:
+npm i
+node index.js OR nodemon index.js
---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+const port = 3000;
+
+let employees = [
+  { id: 1, name: "John Doe", role: "Software Engineer" },
+  { id: 2, name: "Jane Doe", role: "Project Manager" },
+];
+
+app.get("/", (req, res) => {
+  res.send("This is an employee management system");
+});
+
+// To view all employee info
+app.get("/employees", (req, res) => {
+  res.render("employees", { employees });
+});
+
+// To view individual employee info
+app.get("/employees/:id", (req, res) => {
+  const employee = employees.find((emp) => emp.id == req.params.id);
+  if (employee) {
+    res.json(employee);
+  } else {
+    res.status(404).json({ message: "Employee not found" });
+  }
+});
+
+// To create a new employee
+app.post("/employees", (req, res) => {
+  const { name, role } = req.body;
+  const newEmployee = { id: employees.length + 1, name, role };
+  employees.push(newEmployee);
+  res.redirect("/employees");
+});
+
+// To update a new employee
+app.post("/employees/:id/update", (req, res) => {
+  const { id } = req.params;
+  const { name, role } = req.body;
+  const employee = employees.find((emp) => emp.id == id);
+
+  if (employee) {
+    employee.name = name || employee.name;
+    employee.role = role || employee.role;
+    res.redirect("/employees");
+  } else {
+    res.status(404).json({ message: "Employee not found" });
+  }
+});
+
+// To delete a new employee
+app.post("/employees/:id/delete", (req, res) => {
+  employees = employees.filter((emp) => emp.id != req.params.id);
+  res.redirect("/employees");
+});
+
+app.listen(port, () => {
+  console.log(`Server started running on port ${port}`);
+});
